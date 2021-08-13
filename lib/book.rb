7acf9375f470d641cfb80428ca01c53b1a2390e2
sam <surickj@gmail.com>
2021-08-13 00:04:06 -0700

    commit tree a8e9c68cee585f0f5914d26255a9647894326805

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
 
+    class Book
+        attr_reader :title, :author, :page_count, :genre
+        def 
+            initialize title
+            @title=title
+            @author=author
+            @page_count=page_count
 
+        end
+      
+           
+        end
+        
+
+
+  
